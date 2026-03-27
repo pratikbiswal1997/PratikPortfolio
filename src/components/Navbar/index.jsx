f829a36ac067de66f4c0208758bcc82e4a65475e
pratikbiswal1997 <pratikbiswal1997@gmail.com>
2026-03-27 11:57:00 +0530

    fix logo path

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import "./Navbar.css";
-import Logo from "../../assets/Logo";
+import Logo from "../../assets/Logo.jsx";
 import { FaTimes } from "react-icons/fa";
 import { FaArrowUpRightFromSquare, FaBarsStaggered } from "react-icons/fa6";
 import { menu } from "../../data";
